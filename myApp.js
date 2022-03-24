7eba4e1c13def0b3328d46bb938cbb2c09af56a6
Jader Neira <jader.neira@gmail.com>
2022-03-23 22:07:13 -0500

    add findAndUpdate functionality

--- myApp.js
+++ myApp.js
@@ -78,7 +78,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, personUpdated)=>{
+    if(err) return console.error(err);
+    done(null, personUpdated)
+  })
 };
 
 const removeById = (personId, done) => {
