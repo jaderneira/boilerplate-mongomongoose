445e765c808a5802770f6e2d63f1d549cae57a82
Jader Neira <jader.neira@gmail.com>
2022-03-23 21:12:32 -0500

    add createAndSavePerson functionality

--- myApp.js
+++ myApp.js
@@ -7,7 +7,17 @@ const PersonSchema = require('./schemas/personSchema');
 let Person = PersonSchema
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const janeFonda = new Person({
+    name: "Jane Fonda",
+    age: 84,
+    favoriteFoods: ["eggs", "fish", "fresh fruit"]
+  });
+
+  janeFonda.save(function(err, data){
+    if(err) return console.error(err);
+    done(null, data)
+  });
+
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
